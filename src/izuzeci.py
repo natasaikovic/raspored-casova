e22059662ef29d92be683f8e68519c43afc32977
natasaikovic <natasaikovic@ldavico.edu.rs>
2026-08-30 13:04:57 +0200

    Ograniči pauze samo za Ivanu i Jelenu

--- src/izuzeci.py
+++ src/izuzeci.py
@@ -6,21 +6,12 @@ SOLFEDJO_PETI_CAS = frozenset({
     ("Јелена Михаиловић Красић", "43"),
 })
 
-# Potvrđeni izuzeci od nedeljnog ograničenja pauza. Za ove osobe pauze i dalje
-# ulaze u cilj kvaliteta, ali njihov broj i trajanje nisu čvrsta ograničenja.
-IZUZECI_OD_OGRANICENJA_PAUZA = frozenset({
-    "Анастасиа Античевић",
-    "Бранислава Порчић",
-    "Ива Бојовић Петковић",
-    "Ксенија Дукић",
-    "Лидија Палчић",
-    "Марија Вученовић",
-    "Мирјана Анђелковић",
-    "Нина Анђић",
-    "Петар Ђорчевски",
-    "Владимир Јовановић",
-    "Ђорђе Михајловић",
-    "Ђорђина Убовић",
+# Samo ove dve osobe zadržavaju čvrsto ograničenje: najviše jedna pauza
+# nedeljno, duga najviše dva bloka. Za sve ostale pauze ostaju deo cilja
+# kvaliteta, ali njihov broj i trajanje nisu čvrsta ograničenja.
+STROGO_OGRANICENE_PAUZE = frozenset({
+    "Ивана Љујић",
+    "Јелена Првуловић",
 })
 
 
@@ -36,4 +27,4 @@ def dozvoljen_peti_cas_solfedja(predmet: str, nastavnik: str, odeljenja) -> bool
 def izuzet_od_ogranicenja_pauza(osoba: str) -> bool:
     """Da li osoba sme imati više pauza ili pauzu dužu od dva bloka."""
 
-    return osoba in IZUZECI_OD_OGRANICENJA_PAUZA
+    return osoba not in STROGO_OGRANICENE_PAUZE
